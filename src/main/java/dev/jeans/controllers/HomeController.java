88fbf95c07de5a3af84be36565c06f163a20d362
Jean Paulo da Silva <jean.silva@itai.org.br>
2015-06-30 16:05:00 -0300

    Tipo de Produto

--- src/main/java/dev/jeans/controllers/HomeController.java
+++ src/main/java/dev/jeans/controllers/HomeController.java
@@ -1,4 +1,11 @@
 package dev.jeans.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        return "Sptring Boot on Heroku";
+    }
+
 }
